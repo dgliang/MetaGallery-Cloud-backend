39f350a5d45557899b3b354880a49561e6ea1d0f
dgliang <2433353370@qq.com>
2024-10-30 09:57:48 +0800

    update main.go, rotues/router.go, and add a middleware

--- routes/routers.go
+++ routes/routers.go
@@ -9,16 +9,7 @@ import (
 )
 
 func Router(r *gin.Engine) {
-	//r := gin.Default()
-	fmt.Println(1) // 打印数字 1
-	// 添加静态文件服务：假设 public 文件夹中包含 register.html 和其他静态文件
-	// r.Static("/public", "./public")
-	// r.StaticFile("/favicon.ico", "./public/favicon.ico")
 
-	// r.POST("/api/register", func(c *gin.Context) { // 测试是否能命中 /api/register
-	// 	fmt.Println("Static /api/register endpoint hit")
-	// 	c.String(200, "Static test successful")
-	// })
 	// 请求接口都在 “/api” 的目录中
 	api := r.Group("/api")
 	{
@@ -34,6 +25,4 @@ func Router(r *gin.Engine) {
 		api.POST("/updatePassword", controllers.UerController{}.UpdateUserPassword)
 		api.POST("/updateProfile", controllers.UerController{}.UpdateUserInfo)
 	}
-	fmt.Println(2)
-	//return r
 }
